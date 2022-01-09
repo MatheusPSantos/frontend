03099747ff50f66a693554ec7919424051a79b6b
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2022-01-09 10:14:13 -0300

    update: tasting svelte

---
+++ my-svelte-project/src/components/Card.svelte
@@ -0,0 +1,14 @@
+<div class="cards">
+    <slot />
+</div>
+
+<style>
+    .cards {
+        background-color: white;
+        border-radius: 15px;
+        color: #333;
+        padding: 40px 50px;
+        margin: 20px;
+        position: relative;
+    }
+</style>
---
+++ my-svelte-project/src/components/FeedbackList.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { FeedbackStore } from "../stores";
+    import { fade, scale } from "svelte/transition";
+    import FeedbackItem from "./FeedbackItem.svelte";
+    import { onDestroy, onMount } from "svelte";
+
+    // let feedback = [];
+
+    // FeedbackStore.subscribe((data) => (feedback = data));
+
+    // const unsubscribe = FeedbackStore.onMount(() => {
+    //     console.log("mounted");
+    // });
+
+    // onDestroy(() => {});
+</script>
+
+{#each $FeedbackStore as fb (fb.id)}
+    <div in:scale out:fade>
+        <FeedbackItem item={fb} />
+    </div>
+{/each}
