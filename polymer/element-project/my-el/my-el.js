ccab576462afb305237d90df2a12b4a8c9ed983e
matheuspsantos <matheuspereira817@gmail.com>
2022-04-22 13:43:47 -0300

    polymer: element project

---
+++ polymer/element-project/my-el/my-el.js
@@ -0,0 +1,33 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@polymer/paper-button/paper-button.js';
+
+/**
+ * `my-el`
+ * 
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class MyEl extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+      </style>
+      <h2>Hello [[prop1]]!</h2>
+    `;
+  }
+  static get properties() {
+    return {
+      prop1: {
+        type: String,
+        value: 'my-el',
+      },
+    };
+  }
+}
+
+window.customElements.define('my-el', MyEl);
